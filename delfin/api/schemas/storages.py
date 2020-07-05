07f0f70cb742669138655020e25776904fbf215f
sfzeng <zengshufang@huawei.com>
2020-07-05 10:49:06 +0800

    Update access_info model to support more than one access type.

--- delfin/api/schemas/storages.py
+++ delfin/api/schemas/storages.py
@@ -18,12 +18,38 @@ from delfin.api.validation import parameter_types
 create = {
     'type': 'object',
     'properties': {
-        'host': parameter_types.hostname_or_ip_address,
-        'port': parameter_types.tcp_udp_port,
-        'username': {'type': 'string', 'minLength': 1, 'maxLength': 255},
-        'password': {'type': 'string', 'minLength': 1, 'maxLength': 255},
         'vendor': {'type': 'string', 'minLength': 1, 'maxLength': 255},
         'model': {'type': 'string', 'minLength': 1, 'maxLength': 255},
+        'rest_access': {
+            'type': 'object',
+            'properties': {
+                'host': parameter_types.hostname_or_ip_address,
+                'port': parameter_types.tcp_udp_port,
+                'username': {'type': 'string', 'minLength': 1,
+                             'maxLength': 255},
+                'password': {'type': 'string', 'minLength': 1,
+                             'maxLength': 255}
+            },
+            'required': ['host', 'port', 'username', 'password'],
+            'additionalProperties': False
+        },
+        'ssh_access': {
+            'type': 'object',
+            'properties': {
+                'host': parameter_types.hostname_or_ip_address,
+                'port': parameter_types.tcp_udp_port,
+                'username': {'type': 'string', 'minLength': 1,
+                             'maxLength': 255},
+                'password': {'type': 'string', 'minLength': 1,
+                             'maxLength': 255},
+                'pub_key': {'type': 'string', 'minLength': 1,
+                            'maxLength': 255},
+                'pub_key_type': parameter_types.pub_key_type
+            },
+            'required': ['host', 'port', 'username', 'password', 'pub_key',
+                         'pub_key_type'],
+            'additionalProperties': False
+        },
         'extra_attributes': {
             'type': 'object',
             'patternProperties': {
@@ -33,6 +59,10 @@ create = {
             }
         }
     },
-    'required': ['host', 'port', 'username', 'password', 'vendor', 'model'],
+    'required': ['vendor', 'model'],
+    'anyOf': [
+        {'required': ['rest_access']},
+        {'required': ['ssh_access']}
+    ],
     'additionalProperties': False
 }
